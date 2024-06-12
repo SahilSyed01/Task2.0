ff04304d7f99ce83b23a92c588b7ba48e3d98909
SahilSyed01 <sahilsyed7781@gmail.com>
2024-06-12 13:16:51 +0530

    us

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -2,12 +2,16 @@ package middleware
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/ShreerajShettyK/cognitoJwtAuthenticator"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 )
 
 func Authenticate(next http.Handler) http.Handler {
@@ -27,19 +31,65 @@ func Authenticate(next http.Handler) http.Handler {
 		}
 		uiClientToken := authToken[1]
 
+		// Fetch secrets from Secrets Manager
+		region := "us-east-1" // Set your AWS region here
+		secretName := "myApp/mongo-db-credentials"
+		secret, err := getSecret(region, secretName)
+		if err != nil {
+			log.Printf("Error fetching secret: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if secret == nil || secret.UserPoolID == nil || secret.Region == nil {
+			log.Println("Secret, UserPoolID, or Region is nil")
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
 		// Validate the JWT token
 		ctx := context.Background()
-		region := os.Getenv("REGION")
-		userPoolID := os.Getenv("USER_POOL_ID")
 		tokenString := uiClientToken
 
-		_, err := cognitoJwtAuthenticator.ValidateToken(ctx, region, userPoolID, tokenString)
+		_, err = cognitoJwtAuthenticator.ValidateToken(ctx, *secret.Region, *secret.UserPoolID, tokenString)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("Token validation error: %s", err), http.StatusUnauthorized)
+			log.Printf("Token validation error: %v", err)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
 
 		// Token is valid, proceed with the request
 		next.ServeHTTP(w, r)
 	})
+}
+
+func getSecret(region, secretName string) (*SecretsManagerSecret, error) {
+	sess := session.Must(session.NewSession())
+	svc := secretsmanager.New(sess, &aws.Config{Region: aws.String(region)})
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretName),
+	}
+
+	result, err := svc.GetSecretValue(input)
+	if err != nil {
+		return nil, err
+	}
+
+	if result.SecretString == nil {
+		return nil, fmt.Errorf("secret string is nil")
+	}
+
+	secret := &SecretsManagerSecret{}
+	err = json.Unmarshal([]byte(*result.SecretString), secret)
+	if err != nil {
+		return nil, err
+	}
+
+	return secret, nil
+}
+
+type SecretsManagerSecret struct {
+	UserPoolID *string `json:"USER_POOL_ID"`
+	Region     *string `json:"REGION"`
+	// Add other fields from your secret here
 }
